13847a82664e7b66d82e909e3da84083ecb59fb0
Diego Vasquez Admin <dvadmin@ucefdev.org>
2024-02-08 16:36:36 +0100

    Switched config from script to xml

--- cloudLAPS.ps1
+++ cloudLAPS.ps1
@@ -9,23 +9,9 @@
     https://github.com/ElSrJuez/poorMansCloudLAPS
 #>
 
-$Debug = $False
-$WhatIf = $False                                             # If Enabled, the Password and Accounts will not be changed. KeyVault will be used
-####CONFIG
-$minimumPasswordLength = 21
-$tenantID = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"          # ID of Azure where AZKeyVault is located
-$AZAppID  = "yyyyyyyy-yyyy-yyyy-yyyy-yyyyyyyyyyyy"          # Azure App which permissions to only set Secret
-$AZAppSecret = "zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"   # Secret for AZAppID
-$AZVaultName = "KeyVault-01"                                # Name of AzureKeyVault
-$localAdminName = "superman"                                # if set, lokal admin will be renamed
-$removeOtherLocalAdmins = $False                            # if set to True, will remove ALL other local admins, including those set through AzureAD device settings
-$doNotRunOnServers = $True                                  # buildin protection in case an admin accidentally assigns this script to e.g. a domain controller
-$markerFile = Join-Path $Env:TEMP -ChildPath "simpleLAPS.marker"
+$Config = Import-Clixml CloudLAPS.xml
+$markerFile = Join-Path . -ChildPath $Config.markerfile
 $markerFileExists = (Test-Path $markerFile)
-$approvedAdmins = @( #specify SID's for Azure groups such as Global Admins and Device Administrators or for local or domain users to not remove from local admins. These are specific to your tenant, you can get them on a device by running: ([ADSI]::new("WinNT://$($env:COMPUTERNAME)/$((New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-544")).Translate([System.Security.Principal.NTAccount]).Value.Split("\")[1]),Group")).Invoke('Members') | % {"$((New-Object -TypeName System.Security.Principal.SecurityIdentifier -ArgumentList @([Byte[]](([ADSI]$_).properties.objectSid).Value, 0)).Value) <--- a.k.a: $(([ADSI]$_).Path.Split("/")[-1])"}
-"S-1-12-1-2296310142-aaaaaaaaaa-aaaaaaaaa-aaaaaaaaaa"
-"S-1-12-1-465010940-bbbbbbbbbb-bbbbbbbbbb-bbbbbbbbb"
-)
 
 #===============================================================================
 function Connect-AZKeyVault {
@@ -50,6 +36,7 @@ function Connect-AZKeyVault {
         return $false
     }
 }
+
 #===============================================================================
 function Write-AZKeyVaultSecret {
     param (
