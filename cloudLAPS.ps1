f9745de10300d8a40b4a99f3756e115bb11dbcc0
Diego Vasquez <dvasquez@unicef.org>
2024-02-09 10:59:05 +0100

    Various fixes around WhatIf value, added setting to rename local admin account, added setting for passwordNeverExpires value

--- cloudLAPS.ps1
+++ cloudLAPS.ps1
@@ -110,31 +110,16 @@ Remove-Variable identity, principal -ErrorAction SilentlyContinue
 $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
 $principal = New-Object Security.Principal.WindowsPrincipal -ArgumentList $identity
 if ($principal.IsInRole( [Security.Principal.WindowsBuiltInRole]::Administrator )) {
-    Write-CustomEventLog "CloudLAPS starting on $($ENV:COMPUTERNAME) as $($MyInvocation.MyCommand.Name) under Identity $($identity.name)."
-
+    Write-CustomEventLog "CloudLAPS starting on $($ENV:COMPUTERNAME) with Invocation Command '$($MyInvocation.MyCommand)' under Identity $($identity.name)."
 }
 else {
     write-host "Identity $($identity.name) was determined not to be Administrator, exiting."
     return
 }
 
-<#
-deprecate server check
-#===============================================================================
-
-if($doNotRunOnServers -and (Get-WmiObject -Class Win32_OperatingSystem).ProductType -ne 1){
-    Write-CustomEventLog "Unsupported OS!"
-    Write-Error "Unsupported OS!"
-    Exit 0
-}
-#===============================================================================
-#===============================================================================
-
-#>
-
 $Config = Import-Clixml CloudLAPS.xml
-$markerFile = Join-Path . -ChildPath $Config.markerfile
-$markerFileExists = (Test-Path $markerFile)
+#$markerFile = Join-Path . -ChildPath $Config.markerfile
+#$markerFileExists = (Test-Path $markerFile)
 
 if (($Config.AZVaultName | Get-Member).TypeName -eq 'System.String' ) {
     Write-CustomEventLog "CloudLAPS read AZ Vault Name $($Config.AZVaultName) from configuration file."
@@ -144,46 +129,23 @@ else {
     return
 }
 
-<#
-We're not in Intune - may not be needed
-$mode = $MyInvocation.MyCommand.Name.Split(".")[0]
-
-
-#when in remediation mode, always exit successfully as we remediated during the detection phase
-if($mode -ne "detect" -and -not $Config.Debug ){
-    Exit 0
-}else{
-    #check if marker file present, which means we're in the 2nd detection run where nothing should happen except posting the new password to Intune
-    if($markerFileExists){
-        Remove-Item -Path $markerFile -Force -Confirm:$False
-        #ensure the plaintext password is removed from Intune log files and registry (which are written after a delay):
-        $triggers = @((New-ScheduledTaskTrigger -At (get-date).AddMinutes(5) -Once),(New-ScheduledTaskTrigger -At (get-date).AddMinutes(10) -Once),(New-ScheduledTaskTrigger -At (get-date).AddMinutes(30) -Once))
-        $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ex bypass -EncodedCommand RgB1AG4AYwB0AGkAbwBuACAAVwByAGkAdABlAC0AQwB1AHMAdABvAG0ARQB2AGUAbgB0AEwAbwBnACgAJABNAGUAcwBzAGEAZwBlACkAewANAAoAIAAgACAAIAAkAEUAdgBlAG4AdABTAG8AdQByAGMAZQA9ACIALgBzAGkAbQBwAGwAZQBMAEEAUABTACIADQAKACAAIAAgACAAaQBmACAAKABbAFMAeQBzAHQAZQBtAC4ARABpAGEAZwBuAG8AcwB0AGkAYwBzAC4ARQB2AGUAbgB0AEwAbwBnAF0AOgA6AEUAeABpAHMAdABzACgAJwBBAHAAcABsAGkAYwBhAHQAaQBvAG4AJwApACAALQBlAHEAIAAkAEYAYQBsAHMAZQAgAC0AbwByACAAWwBTAHkAcwB0AGUAbQAuAEQAaQBhAGcAbgBvAHMAdABpAGMAcwAuAEUAdgBlAG4AdABMAG8AZwBdADoAOgBTAG8AdQByAGMAZQBFAHgAaQBzAHQAcwAoACQARQB2AGUAbgB0AFMAbwB1AHIAYwBlACkAIAAtAGUAcQAgACQARgBhAGwAcwBlACkAewANAAoAIAAgACAAIAAgACAAIAAgAE4AZQB3AC0ARQB2AGUAbgB0AEwAbwBnACAALQBMAG8AZwBOAGEAbQBlACAAQQBwAHAAbABpAGMAYQB0AGkAbwBuACAALQBTAG8AdQByAGMAZQAgACQARQB2AGUAbgB0AFMAbwB1AHIAYwBlACAAfAAgAE8AdQB0AC0ATgB1AGwAbAANAAoAIAAgACAAIAB9AA0ACgAgACAAIAAgAFcAcgBpAHQAZQAtAEUAdgBlAG4AdABMAG8AZwAgAC0ATABvAGcATgBhAG0AZQAgAEEAcABwAGwAaQBjAGEAdABpAG8AbgAgAC0AUwBvAHUAcgBjAGUAIAAkAEUAdgBlAG4AdABTAG8AdQByAGMAZQAgAC0ARQBuAHQAcgB5AFQAeQBwAGUAIABJAG4AZgBvAHIAbQBhAHQAaQBvAG4AIAAtAEUAdgBlAG4AdABJAGQAIAAxADkAOAA1ACAALQBNAGUAcwBzAGEAZwBlACAAJABNAGUAcwBzAGEAZwBlACAAfAAgAE8AdQB0AC0ATgB1AGwAbAANAAoAfQANAAoADQAKACMAdwBpAHAAZQAgAHAAYQBzAHMAdwBvAHIAZAAgAGYAcgBvAG0AIABsAG8AZwBmAGkAbABlAHMADQAKAHQAcgB5AHsADQAKACAAIAAgACAAJABpAG4AdAB1AG4AZQBMAG8AZwAxACAAPQAgAEoAbwBpAG4ALQBQAGEAdABoACAAJABFAG4AdgA6AFAAcgBvAGcAcgBhAG0ARABhAHQAYQAgAC0AYwBoAGkAbABkAHAAYQB0AGgAIAAiAE0AaQBjAHIAbwBzAG8AZgB0AFwASQBuAHQAdQBuAGUATQBhAG4AYQBnAGUAbQBlAG4AdABFAHgAdABlAG4AcwBpAG8AbgBcAEwAbwBnAHMAXABBAGcAZQBuAHQARQB4AGUAYwB1AHQAbwByAC4AbABvAGcAIgANAAoAIAAgACAAIAAkAGkAbgB0AHUAbgBlAEwAbwBnADIAIAA9ACAASgBvAGkAbgAtAFAAYQB0AGgAIAAkAEUAbgB2ADoAUAByAG8AZwByAGEAbQBEAGEAdABhACAALQBjAGgAaQBsAGQAcABhAHQAaAAgACIATQBpAGMAcgBvAHMAbwBmAHQAXABJAG4AdAB1AG4AZQBNAGEAbgBhAGcAZQBtAGUAbgB0AEUAeAB0AGUAbgBzAGkAbwBuAFwATABvAGcAcwBcAEkAbgB0AHUAbgBlAE0AYQBuAGEAZwBlAG0AZQBuAHQARQB4AHQAZQBuAHMAaQBvAG4ALgBsAG8AZwAiAA0ACgAgACAAIAAgAGkAZgAgACgAIABUAGUAcwB0AC0AUABhAHQAaAAgAC0ATABpAHQAZQByAGEAbABQAGEAdABoACAAJABpAG4AdAB1AG4AZQBMAG8AZwAxACAALQBQAGEAdABoAFQAeQBwAGUAOgBMAGUAYQBmACAAKQAgAHsADQAKAAkAIAAgACAAIABTAGUAdAAtAEMAbwBuAHQAZQBuAHQAIAAtAEYAbwByAGMAZQAgAC0AQwBvAG4AZgBpAHIAbQA6ACQARgBhAGwAcwBlACAALQBQAGEAdABoACAAJABpAG4AdAB1AG4AZQBMAG8AZwAxACAALQBWAGEAbAB1AGUAIAAoAEcAZQB0AC0AQwBvAG4AdABlAG4AdAAgAC0AUABhAHQAaAAgACQAaQBuAHQAdQBuAGUATABvAGcAMQAgAHwAIABTAGUAbABlAGMAdAAtAFMAdAByAGkAbgBnACAALQBQAGEAdAB0AGUAcgBuACAAIgBQAGEAcwBzAHcAbwByAGQAIgAgAC0ATgBvAHQATQBhAHQAYwBoACkADQAKACAAIAAgACAAfQANAAoAIAAgACAAIABpAGYAIAAoACAAVABlAHMAdAAtAFAAYQB0AGgAIAAtAEwAaQB0AGUAcgBhAGwAUABhAHQAaAAgACQAaQBuAHQAdQBuAGUATABvAGcAMgAgAC0AUABhAHQAaABUAHkAcABlADoATABlAGEAZgAgACkAIAB7AA0ACgAgACAAIAAgACAAIAAgACAAUwBlAHQALQBDAG8AbgB0AGUAbgB0ACAALQBGAG8AcgBjAGUAIAAtAEMAbwBuAGYAaQByAG0AOgAkAEYAYQBsAHMAZQAgAC0AUABhAHQAaAAgACQAaQBuAHQAdQBuAGUATABvAGcAMgAgAC0AVgBhAGwAdQBlACAAKABHAGUAdAAtAEMAbwBuAHQAZQBuAHQAIAAtAFAAYQB0AGgAIAAkAGkAbgB0AHUAbgBlAEwAbwBnADIAIAB8ACAAUwBlAGwAZQBjAHQALQBTAHQAcgBpAG4AZwAgAC0AUABhAHQAdABlAHIAbgAgACIAUABhAHMAcwB3AG8AcgBkACIAIAAtAE4AbwB0AE0AYQB0AGMAaAApAA0ACgAgACAAIAAgAH0ADQAKAH0AYwBhAHQAYwBoAHsAJABOAHUAbABsAH0ADQAKAA0ACgAjAG8AbgBsAHkAIAB3AGkAcABlACAAcgBlAGcAaQBzAHQAcgB5ACAAZABhAHQAYQAgAGEAZgB0AGUAcgAgAGQAYQB0AGEAIABoAGEAcwAgAGIAZQBlAG4AIABzAGUAbgB0ACAAdABvACAATQBzAGYAdAANAAoAaQBmACAAKAAgACQAKABUAGUAcwB0AC0AUABhAHQAaAAgAC0ATABpAHQAZQByAGEAbABQAGEAdABoACAAJABpAG4AdAB1AG4AZQBMAG8AZwAyACAALQBQAGEAdABoAFQAeQBwAGUAOgBMAGUAYQBmACkAIAAtAGEAbgBkACAAJAAoAFQAZQBzAHQALQBQAGEAdABoACAALQBQAGEAdABoACAAIgBIAEsATABNADoAXABTAG8AZgB0AHcAYQByAGUAXABNAGkAYwByAG8AcwBvAGYAdABcAEkAbgB0AHUAbgBlAE0AYQBuAGEAZwBlAG0AZQBuAHQARQB4AHQAZQBuAHMAaQBvAG4AXABTAGkAZABlAEMAYQByAFAAbwBsAGkAYwBpAGUAcwBcAFMAYwByAGkAcAB0AHMAXABSAGUAcABvAHIAdABzACIAKQAgACkAIAB7AA0ACgAgACAAIAAgAGkAZgAoACgARwBlAHQALQBDAG8AbgB0AGUAbgB0ACAALQBQAGEAdABoACAAJABpAG4AdAB1AG4AZQBMAG8AZwAyACAAfAAgAFMAZQBsAGUAYwB0AC0AUwB0AHIAaQBuAGcAIAAtAFAAYQB0AHQAZQByAG4AIAAiAFAAbwBsAGkAYwB5ACAAcgBlAHMAdQBsAHQAcwAgAGEAcgBlACAAcwB1AGMAYwBlAHMAcwBmAHUAbABsAHkAIABzAGUAbgB0AC4AIgApACkAewANAAoAIAAgACAAIAAgACAAIAAgAFcAcgBpAHQAZQAtAEMAdQBzAHQAbwBtAEUAdgBlAG4AdABMAG8AZwAgACIASQBuAHQAdQBuAGUAIABsAG8AZwBmAGkAbABlACAAaQBuAGQAaQBjAGEAdABlAHMAIABzAGMAcgBpAHAAdAAgAHIAZQBzAHUAbAB0AHMAIABoAGEAdgBlACAAYgBlAGUAbgAgAHIAZQBwAG8AcgB0AGUAZAAgAHQAbwAgAE0AaQBjAHIAbwBzAG8AZgB0ACIADQAKACAAIAAgACAAIAAgACAAIABTAGUAdAAtAEMAbwBuAHQAZQBuAHQAIAAtAEYAbwByAGMAZQAgAC0AQwBvAG4AZgBpAHIAbQA6ACQARgBhAGwAcwBlACAALQBQAGEAdABoACAAJABpAG4AdAB1AG4AZQBMAG8AZwAyACAALQBWAGEAbAB1AGUAIAAoAEcAZQB0AC0AQwBvAG4AdABlAG4AdAAgAC0AUABhAHQAaAAgACQAaQBuAHQAdQBuAGUATABvAGcAMgAgAHwAIABTAGUAbABlAGMAdAAtAFMAdAByAGkAbgBnACAALQBQAGEAdAB0AGUAcgBuACAAIgBQAG8AbABpAGMAeQAgAHIAZQBzAHUAbAB0AHMAIABhAHIAZQAgAHMAdQBjAGMAZQBzAHMAZgB1AGwAbAB5ACAAcwBlAG4AdAAuACIAIAAtAE4AbwB0AE0AYQB0AGMAaAApAA0ACgAgACAAIAAgACAAIAAgACAAUwB0AGEAcgB0AC0AUwBsAGUAZQBwACAALQBzACAAOQAwAA0ACgAgACAAIAAgACAAIAAgACAAdAByAHkAewANAAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAZgBvAHIAZQBhAGMAaAAoACQAVABlAG4AYQBuAHQAIABpAG4AIAAoAEcAZQB0AC0AQwBoAGkAbABkAEkAdABlAG0AIAAiAEgASwBMAE0AOgBcAFMAbwBmAHQAdwBhAHIAZQBcAE0AaQBjAHIAbwBzAG8AZgB0AFwASQBuAHQAdQBuAGUATQBhAG4AYQBnAGUAbQBlAG4AdABFAHgAdABlAG4AcwBpAG8AbgBcAFMAaQBkAGUAQwBhAHIAUABvAGwAaQBjAGkAZQBzAFwAUwBjAHIAaQBwAHQAcwBcAFIAZQBwAG8AcgB0AHMAIgApACkAewANAAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIABmAG8AcgBlAGEAYwBoACgAJABzAGMAcgBpAHAAdAAgAGkAbgAgACgARwBlAHQALQBDAGgAaQBsAGQASQB0AGUAbQAgACQAVABlAG4AYQBuAHQALgBQAFMAUABhAHQAaAApACkAewANAAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACQAagBzAG8AbgAgAD0AIAAoACgARwBlAHQALQBJAHQAZQBtAFAAcgBvAHAAZQByAHQAeQAgAC0AUABhAHQAaAAgACgASgBvAGkAbgAtAFAAYQB0AGgAIAAkAHMAYwByAGkAcAB0AC4AUABTAFAAYQB0AGgAIAAtAEMAaABpAGwAZABQAGEAdABoACAAIgBSAGUAcwB1AGwAdAAiACkAIAAtAE4AYQBtAGUAIAAiAFIAZQBzAHUAbAB0ACIAKQAuAFIAZQBzAHUAbAB0ACAAfAAgAGMAbwBuAHYAZQByAHQAZgByAG8AbQAtAGoAcwBvAG4AKQANAAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgAGkAZgAoACQAagBzAG8AbgAuAFAAbwBzAHQAUgBlAG0AZQBkAGkAYQB0AGkAbwBuAEQAZQB0AGUAYwB0AFMAYwByAGkAcAB0AE8AdQB0AHAAdQB0AC4AUwB0AGEAcgB0AHMAVwBpAHQAaAAoACIAcwBpAG0AcABsAGUATABBAFAAUwAiACkAKQB7AA0ACgAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAkAGoAcwBvAG4ALgBQAG8AcwB0AFIAZQBtAGUAZABpAGEAdABpAG8AbgBEAGUAdABlAGMAdABTAGMAcgBpAHAAdABPAHUAdABwAHUAdAAgAD0AIAAiAFIARQBEAEEAQwBUAEUARAAiAA0ACgAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIABTAGUAdAAtAEkAdABlAG0AUAByAG8AcABlAHIAdAB5ACAALQBQAGEAdABoACAAKABKAG8AaQBuAC0AUABhAHQAaAAgACQAcwBjAHIAaQBwAHQALgBQAFMAUABhAHQAaAAgAC0AQwBoAGkAbABkAFAAYQB0AGgAIAAiAFIAZQBzAHUAbAB0ACIAKQAgAC0ATgBhAG0AZQAgACIAUgBlAHMAdQBsAHQAIgAgAC0AVgBhAGwAdQBlACAAKAAkAGoAcwBvAG4AIAB8ACAAQwBvAG4AdgBlAHIAdABUAG8ALQBKAHMAbwBuACAALQBEAGUAcAB0AGgAIAAxADAAIAAtAEMAbwBtAHAAcgBlAHMAcwApACAALQBGAG8AcgBjAGUAIAAtAEMAbwBuAGYAaQByAG0AOgAkAEYAYQBsAHMAZQANAAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAVwByAGkAdABlAC0AQwB1AHMAdABvAG0ARQB2AGUAbgB0AEwAbwBnACAAIgByAGUAZABhAGMAdABlAGQAIABhAGwAbAAgAGwAbwBjAGEAbAAgAGQAYQB0AGEAIgANAAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgAH0ADQAKACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAIAAgACAAfQANAAoAIAAgACAAIAAgACAAIAAgACAAIAAgACAAfQANAAoAIAAgACAAIAAgACAAIAAgAH0AYwBhAHQAYwBoAHsAJABOAHUAbABsAH0ADQAKACAAIAAgACAAfQANAAoAfQA=" #base64 UTF16-LE encoded command https://www.base64encode.org/
-        $Null = Register-ScheduledTask -TaskName "simpleLAPS_WL" -Trigger $triggers -User "SYSTEM" -Action $Action -Force
-
-        Write-Host "simpleLAPS changed password for $($localAdminName) on $(Get-Date)"
-        Exit 0
-    }
-}
-#>
-
 $Error.Clear()
 
 Remove-Variable localAdmin, BlackHole -ErrorAction SilentlyContinue
 try{    
     $localAdmin = Get-LocalUser | Where-Object { $_.SID.Value.EndsWith("-500") }
-    if ( $Config.localAdminName -and $localAdmin.Name -ne $Config.localAdminName) {
+    if ( $Config.localAdminName -and ($true -eq $Config.renameAdminAccount) -and $localAdmin.Name -ne $Config.localAdminName) {
         Write-CustomEventLog "Rename local Administrator from '$($localAdmin.Name)' to '$($Config.localAdminName)'"        
         $BlackHole = Get-LocalUser -Name $Config.localAdminName -ErrorAction:SilentlyContinue
         if ( $BlackHole ) {
             Write-CustomEventLog "Remove preexisting '$($localAdmin.Name)' '$($BlackHole.SID.Value)'"
-            Remove-LocalUser -SID $BlackHole.SID.Value -Confirm:$False -WhatIf:$WhatIf | Out-Null
+            Remove-LocalUser -SID $BlackHole.SID.Value -Confirm:$False -WhatIf:$Config.WhatIf | Out-Null
         }
-        Rename-LocalUser -SID $localAdmin.SID.Value -NewName $Config.localAdminName -Confirm:$false -WhatIf:$WhatIf | Out-Null
+        Rename-LocalUser -SID $localAdmin.SID.Value -NewName $Config.localAdminName -Confirm:$false -WhatIf:$Config.WhatIf | Out-Null
         $localAdmin = Get-LocalUser -SID $localAdmin.SID.Value
     }
     else {
-        Write-CustomEventLog "No need to rename local Administrator from '$($localAdmin.Name)' to '$($Config.localAdminName)'." 
+        Write-CustomEventLog "With renameAdminAccount set to '$($Config.renameAdminAccount)', no need to rename local Administrator from '$($localAdmin.Name)' to '$($Config.localAdminName)'." 
     }
 
     if ( -not $localAdmin.Enabled ) {
@@ -191,7 +153,7 @@ try{
         Enable-LocalUser -SID $localAdmin.SID.Value -WhatIf:$WhatIf | Out-Null
     }
     else {
-        Write-CustomEventLog "No need to enable local Administrator '$($localAdmin.Name)'." 
+        Write-CustomEventLog "With Enabled attribute already at '$($localAdmin.Enabled)', no need to enable local Administrator '$($localAdmin.Name)'." 
     }
     if(!$localAdmin){Throw}
 }catch{
@@ -200,19 +162,20 @@ try{
     Exit 1
 }
 
-Remove-Variable administratorsGroupName, group, administrators -ErrorAction SilentlyContinue
+Remove-Variable administratorsGroupName, group, administrators,adminNames -ErrorAction SilentlyContinue
 try{
     $administratorsGroupName = (New-Object System.Security.Principal.SecurityIdentifier("S-1-5-32-544")).Translate([System.Security.Principal.NTAccount]).Value.Split("\")[1]
     Write-CustomEventLog "Local Administrators group is called $administratorsGroupName"
     $group = [ADSI]::new("WinNT://$($env:COMPUTERNAME)/$($administratorsGroupName),Group")
     $administrators = $group.Invoke('Members') | ForEach-Object {(New-Object -TypeName System.Security.Principal.SecurityIdentifier -ArgumentList @([Byte[]](([ADSI]$_).properties.objectSid).Value, 0)).Value}
-    
-    Write-CustomEventLog "There are $($administrators.count) readable accounts in $administratorsGroupName"
+    $adminNames = $administrators | 
+        foreach {([System.Security.Principal.SecurityIdentifier]$_).translate([System.Security.Principal.NTAccount])}
+    Write-CustomEventLog "There are $($administrators.count) readable accounts in $administratorsGroupName with values '$($adminNames.value)'."
 
     if(!$administrators -or $administrators -notcontains $localAdmin.SID.Value){
         Write-CustomEventLog "$($Config.localAdminName) is not a local administrator, adding..."
         Add-LocalGroupMember -Group $administratorsGroupName -Member $Config.localAdminName -Confirm:$False -ErrorAction Stop -WhatIf:$WhatIf | Out-Null
-        Write-CustomEventLog "Added $($Config.localAdminName) to the local administrators group"
+        Write-CustomEventLog "Added $($Config.localAdminName) to the local administrators group, WhatIf mode '$($Config.WhatIf)"
     }
     #remove other local admins if specified, only executes if adding the new local admin succeeded
     if($Config.removeOtherLocalAdmins){
@@ -223,8 +186,8 @@ try{
             }
             if($administrator -ne $localAdmin.SID.Value -and $Config.approvedAdmins -notcontains $administrator){
                 Write-CustomEventLog "removeOtherLocalAdmins set to True, removing $($administrator) from Local Administrators"
-                Remove-LocalGroupMember -Group $administratorsGroupName -Member $administrator -Confirm:$False -WhatIf:$WhatIf | Out-Null
-                Write-CustomEventLog "Removed $administrator from Local Administrators"
+                Remove-LocalGroupMember -Group $administratorsGroupName -Member $administrator -Confirm:$False -WhatIf:$Config.WhatIf | Out-Null
+                Write-CustomEventLog "Removed account '$administrator' from Local '$($administratorsGroupName)' group, WhatIf mode '$($Config.WhatIf)"
             }else{
                 Write-CustomEventLog "Not removing $($administrator) because of whitelisting"
             }
@@ -240,7 +203,7 @@ try{
 
 Remove-Variable newPwd, newPwdSecStr, AZToken, BlackHole -ErrorAction SilentlyContinue
 try{
-    Write-CustomEventLog "Setting password for '$($localAdmin.Name)'..."
+    Write-CustomEventLog "Setting password for '$($localAdmin.Name)', PasswordNeverExpires to '$($Config.PasswordNeverExpires)', WhatIf mode '$($Config.WhatIf)'..."
     $newPwd = Get-NewPassword $Config.minimumPasswordLength
     $newPwdSecStr = ConvertTo-SecureString $newPwd -asplaintext -force    
     $AZToken = Connect-AZKeyVault -tenantId $Config.tenantID -Client_ID $Config.AZAppID -Secret $Config.AZAppSecret
@@ -253,14 +216,15 @@ try{
         Write-CustomEventLog "Unexpected result setting secret name '$($env:COMPUTERNAME)' to azure vault '$($Config.AZVaultName)' with User Name '$($Config.localAdminName)'."    
         throw
         }
-    $localAdmin | Set-LocalUser -Password $newPwdSecStr -Confirm:$False -AccountNeverExpires -PasswordNeverExpires $True -UserMayChangePassword $True -WhatIf:$WhatIf | Out-Null
-    Write-CustomEventLog "Password for $($localAdmin.Name) set to a new value, see AzureKeyVault '$($Config.AZVaultName)'."
+    $localAdmin | Set-LocalUser -Password $newPwdSecStr -Confirm:$False -PasswordNeverExpires $Config.PasswordNeverExpires -WhatIf:$Config.WhatIf | Out-Null
+    Write-CustomEventLog "Password for $($localAdmin.Name) set to a new value, see AzureKeyVault '$($Config.AZVaultName)' with WhatIf mode set to '$($Config.WhatIf)'."
 }catch{
     Write-CustomEventLog "Unexpected error returned trying to set new password for $($localAdmin.Name)"
     Write-Host "Failed to set password for $($localAdmin.Name) because of $($_)"
     # Exit 1
 }
 
-Write-Host "LeanLAPS ran successfully for $($Config.localAdminName)"
-Set-Content -Path $markerFile -Value $Config.localAdminName -Force -Confirm:$False | Out-Null
+Write-Host "CloudLAPS ran successfully for '$($localAdmin.Name)' with WhatIf '$($Config.WhatIf)'."
+Write-CustomEventLog "CloudLAPS ran successfully for '$($localAdmin.Name)' with WhatIf '$($Config.WhatIf)'."
+#Set-Content -Path $markerFile -Value $Config.localAdminName -Force -Confirm:$False | Out-Null
 # Exit 0
