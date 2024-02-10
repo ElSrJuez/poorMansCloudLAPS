8e6310026098ed4de3cafdefc21506664ceb58c7
Diego Vasquez <elsrjuez@live.com>
2024-02-10 10:10:44 +0100

    added state machine for better error handling

--- module/Write-CustomEventLog.ps1
+++ module/Write-CustomEventLog.ps1
@@ -1,13 +1,27 @@
-﻿#===============================================================================
-Function Write-CustomEventLog($Message){
-    # Write to the event log
-    $EventSource=".CloudLAPS"
-    if ([System.Diagnostics.EventLog]::Exists('Application') -eq $False -or [System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False){
-        New-EventLog -LogName Application -Source $EventSource  | Out-Null
+﻿<#
+.Synopsis
+   # Write an event to a Windows event log, create event source if needed
+#>
+function Write-CustomEventLog
+{
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [string]$Message,
+        [string]$EventType = 'Information',
+        [string]$EventSource=".CloudLAPS",
+        [string]$LogName = 'Application',
+        [int]$EventID = 2024,
+        [bool]$LogToHost = $true
+    )    
+    if ([System.Diagnostics.EventLog]::Exists($LogName) -eq $False -or [System.Diagnostics.EventLog]::SourceExists($EventSource) -eq $False) {
+        New-EventLog -LogName $LogName -Source $EventSource  | Out-Null
     }
-    Write-EventLog -LogName Application -Source $EventSource -EntryType Information -EventId 2024 -Message $Message | Out-Null
-    # Also log to console if in debug mode
-    if ($Config.Debug) {
-        Write-Host "$EventSource ID:2024 Message: $Message"
+    Write-EventLog -LogName $LogName -Source $EventSource -EntryType $EventType -EventId $EventID -Message $Message | Out-Null
+    # Also log to console if specified mode
+    if ($LogToHost) {
+        Write-Host "$EventSource ID:$EventID Message: $Message"
     }
 }
